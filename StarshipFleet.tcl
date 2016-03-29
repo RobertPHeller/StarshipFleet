4777219e94bd964c569a443e47f13210106bbbe4
Robert Heller <heller@deepsoft.com>
2016-03-29 17:57:36 +0000

    Refinement of missle launchers.

--- StarshipFleet.tcl
+++ StarshipFleet.tcl
@@ -8,7 +8,7 @@
 #  Author        : $Author$
 #  Created By    : Robert Heller
 #  Created       : Thu Mar 24 12:57:13 2016
-#  Last Modified : <160328.1433>
+#  Last Modified : <160329.1353>
 #
 #  Description	
 #
@@ -40,10 +40,17 @@
 #
 #*****************************************************************************
 
+## @mainpage Starship objects.
+#
+# 
+# @author Robert Heller \<heller\@deepsoft.com\>
+
 
 package require snit
 
-
+proc _ {args} {
+    return [eval format $args]
+}
 
 
 namespace eval starships {
@@ -63,7 +70,7 @@ namespace eval starships {
         destroyer
         ## @brief Destroyer
         lightcrusier
-        ## @brief Light Ccrusier
+        ## @brief Light Crusier
         heavycrusier
         ## @brief Heavy Crusier
         battlecrusier
@@ -165,7 +172,7 @@ namespace eval starships {
             # @par
             
             $self configurelist $args
-            if {[lsearch -exact -- $args -maxaccel] < 0} {
+            if {[lsearch -exact $args -maxaccel] < 0} {
                 $self configure -maxaccel [$self cget -maxdesignaccel]
             }
         }
@@ -209,10 +216,10 @@ namespace eval starships {
         }
     }
     
-    snit::integer LauncherCount -min 4 -max 25
+    snit::integer LauncherCount -min 0 -max 25
     ## @typedef integer LauncherCount
-    # The number of missle launchers, and integer from 4 (small destroyer) to 
-    # 25 (super dreadnought).
+    # The number of missle launchers, and integer from 0 (a non-missle firing 
+    # ship, such as a troop carrier) to 25 (super dreadnought).
     snit::enum LauncherSize -values {
         ## @enum LauncherSize
         # Missle size classes.
@@ -227,6 +234,137 @@ namespace eval starships {
         ## @brief Planet buster missle.
     }
     
+    snit::double radians -max [expr {acos(-1)}] -min [expr {acos(1)}]
+    ## @typedef double radians
+    # A double between \f$0\f$ and \f$\pi\f$.
+    
+    snit::type ThrustVector {
+        ## @brief ThrustVector object, representing a 3D thrust vector.
+        #
+        # This is a 3D vector object representing the thrust vector of an 
+        # object (such as a ship or missle) in space.
+        # 
+        # Options:
+        # @arg -acceleration The fraction of \f$c\f$ of thrust. This is a
+        #            readonly option and defaults to 0.0.
+        # @arg -xang The x angle of the thrust vector as a floating point 
+        #            number in radians between \f$0\f$ and \f$\pi\f$. 
+        #            This is a readonly option and defaults to 0.0.
+        # @arg -yang The y angle of the thrust vector as a floating point 
+        #            number in radians between \f$0\f$ and \f$\pi\f$. 
+        #            This is a readonly option and defaults to 0.0.
+        # @arg -zang The z angle of the thrust vector as a floating point 
+        #            number in radians between \f$0\f$ and \f$\pi\f$. 
+        #            This is a readonly option and defaults to 0.0.
+        # @par
+        
+        variable dx
+        ## @privatesection This is the x component of the thrust vector, in
+        # millions of kilometers per second, squared.
+        variable dy
+        ## This is the y component of the thrust vector, in millions of 
+        # kilometers per second, squared.
+        variable dz
+        ## This is the z component of the thrust vector, in millions of 
+        # kilometers per second, squared.
+        typevariable c 299.792458 
+        ## The speed of light (\f$c\f$) in millions of kilometers per second.
+        option -acceleration -readonly yes -default 0.0 \
+              -type starships::StarshipAcceleration
+        option -xang -readonly yes -default 0.0 -type starships::radians
+        option -yang -readonly yes -default 0.0 -type starships::radians
+        option -zang -readonly yes -default 0.0 -type starships::radians
+        
+        constructor {args} {
+            ## @publicsection Construct a ThrustVector.
+            #
+            # @param name The name of the Thrust Vector.
+            # @param ... Options:
+            # @arg -acceleration The fraction of \f$c\f$ of thrust. This is a
+            #            readonly option and defaults to 0.0.
+            # @arg -xang The x angle of the thrust vector as a floating point 
+            #            number in radians between \f$0\f$ and \f$\pi\f$. 
+            #            This is a readonly option and defaults to 0.0.
+            # @arg -yang The y angle of the thrust vector as a floating point 
+            #            number in radians between \f$0\f$ and \f$\pi\f$. 
+            #            This is a readonly option and defaults to 0.0.
+            # @arg -zang The z angle of the thrust vector as a floating point 
+            #            number in radians between \f$0\f$ and \f$\pi\f$. 
+            #            This is a readonly option and defaults to 0.0.
+            # @par
+            
+            $self configurelist $args
+            # Now for some hairy math...
+            # Math lifted from http://www.intmath.com/vectors/7-vectors-in-3d-space.php
+            set alpha $options(-xang)
+            set beta  $options(-yang)
+            set gamma $options(-zang)
+            
+            set _dx [expr {cos($alpha)}]
+            set _dy [expr {cos($beta)}]
+            set _dz [expr {cos($gamma)}]
+            set ulength [_length $_dx $_dy $_dz]
+            #puts stderr "*** $type create: ulength = $ulength"
+            if {$ulength < 1} {
+                error [_ "Imposible launch angle: %f6.3 %f6.3 %f6.3!" $alpha $beta $gamma]
+            }
+            set adelta [expr {($options(-acceleration) * $c) / $ulength}]
+            set dx [expr {$_dx *  $adelta}]
+            set dy [expr {$_dy *  $adelta}]
+            set dz [expr {$_dz *  $adelta}]
+        }
+        method DeltaX {} {
+            ## Return the delta x component of the thrust vector.
+            #
+            # @return The delta x component.
+            
+            return $dx
+        }
+        method DeltaY {} {
+            ## Return the delta y component of the thrust vector.
+            #
+            # @return The delta y component.
+            
+            return $dy
+        }
+        method DeltaZ {} {
+            ## Return the delta z component of the thrust vector.
+            #
+            # @return The delta z component.
+            
+            return $dz
+        }
+        typemethod validate {object} {
+            ## Validate that the object is a ThrustVector.
+            #
+            
+            if {[catch {$object info type} thetype]} {
+                error [_ "%s is not a %s!" $object $type]
+            } elseif {$thetype ne $type} {
+                error [_ "%s is not a %s!" $object $type]
+            } else {
+                return $object
+            }
+        }
+        method Length {} {
+            ## Return the unit length of the vector.
+            #
+            # @return The unit length of the vector. 
+            
+            return [_length $dx $dy $dz]
+        }
+        proc _length {x y z} {
+            ## @privatesection Compute the length of a 
+            # \f$[x \quad y \quad z]\f$.
+            #
+            # @param x The dx component.
+            # @param y The dy component.
+            # @param z The dz component.
+            # @return \f$\sqrt{x^2+y^2+z^2}\f$
+            
+            return [expr {sqrt(($x*$x) + ($y*$y) + ($z*$z))}]
+        }
+    }
     
     snit::type StarshipMissleLaunchers {
         ## @brief A bank of missle launchers mounted on a starship.
@@ -239,7 +377,7 @@ namespace eval starships {
         # anti-missle defence system.
         #
         # Options
-        # @arg -count The number of launchers, as a integer between 4 and 25. 
+        # @arg -count The number of launchers, as a integer between 0 and 25. 
         #             This is a readonly option. The default is 4.
         # @arg -size  The size class of the launchers, as an enumerated type.
         #             This is a readonly option. The default is mark1.
@@ -254,14 +392,23 @@ namespace eval starships {
         typevariable launcherspecs -array {
             mark1,magazine 20
             mark1,ewpercent 25
+            mark1,acceleration .1
+            mark1,range 1.0
             mark2,magazine 50
             mark2,ewpercent 25
+            mark2,acceleration .15
+            mark2,range 1.25
             mark4,magazine 100
             mark4,ewpercent 25
+            mark4,acceleration .175
+            mark4,range 1.5
             mark10,magazine 20
             mark10,ewpercent 2
+            mark10,acceleration .2
+            mark10,range 1.75
         }
-        ## Launcher specifications: magazine size and percent of EW platforms.
+        ## Launcher specifications: magazine size, percent of EW platforms,
+        # range (10^6 Km), and acceleration (fraction of c).
         
         constructor {args} {
             ## @publicsection Construct banks of missle launchers.
@@ -270,7 +417,7 @@ namespace eval starships {
             #
             # @param name The name of the missle launchers object.
             # @param ... Options:
-            # @arg -count The number of launchers, as a integer between 4 and 25. 
+            # @arg -count The number of launchers, as a integer between 0 and 25. 
             #             This is a readonly option. The default is 4.
             # @arg -size  The size class of the launchers, as an enumerated type.
             #             This is a readonly option. The default is mark1.
@@ -282,13 +429,20 @@ namespace eval starships {
                 set launchers($ilaunch,functional) yes
             }
         }
-        method launch {{number all}} {
+        method launch {xang yang zang {number all}} {
             ## @brief Launch missiles.
             # Launch up to one missle per launcher.  
             #
+            # @param xang The x launch angle in radians, between
+            #             \f$0\f$ and \f$\pi\f$.
+            # @param yang The y launch angle in radians, between
+            #             \f$0\f$ and \f$\pi\f$.
+            # @param zang The z launch angle in radians, between
+            #             \f$0\f$ and \f$\pi\f$.
             # @param number The number of missles to launch or all.  If all, 
             # launch a missle from all launchers that are functional.
-            # @return The missles launched.
+            # @return The ThrustVector, range, missle size with a list of the 
+            # missles launched.
             
             if {$number eq "all"} {set number $options(-count)}
             set launched [list]
@@ -299,7 +453,18 @@ namespace eval starships {
                     set launchers($i,magazine) [lrange $launchers($i,magazine) 1 end]
                 }
             }
-            return $launched
+            if {[llength $launched] == 0} {
+                return $launched
+            } else {
+                set size [$self cget -size]
+                set accel $launcherspecs($size,acceleration)
+                set range $launcherspecs($size,range)
+                return [linsert $launched 0 [starships::ThrustVector $size%AUTO% \
+                                             -acceleration $accel \
+                                             -xang $xang \
+                                             -yang $yang \
+                                             -zang $zang] $range $size]
+            }
         }
         method reload {missles} {
             ## Reload missle magazines from the supply provided.
@@ -313,12 +478,12 @@ namespace eval starships {
             while {[llength $missles] > 0 && 
                 [$self reloadspace] > 0 && 
                 [lsearch -exact $missles w] >= 0} {
-                puts stderr "*** $self reload: missles = $missles, reloadspace is [$self reloadspace]"
+                #puts stderr "*** $self reload: missles = $missles, reloadspace is [$self reloadspace]"
                 set unused [list]
                 set i 0
                 foreach next $missles {
                     set ew [$self _ewpercent]
-                    puts stderr "*** $self reload: i = $i, next = $next, ew = $ew"
+                    #puts stderr "*** $self reload: i = $i, next = $next, ew = $ew"
                     while {!$launchers($i,functional) || 
                         [llength $launchers($i,magazine)] >= $launcherspecs($options(-size),magazine)} {
                         incr i
@@ -493,7 +658,7 @@ namespace eval starships {
             #                      StarshipMissleLaunchers type.
             # @par
             
-            if {[lsearch -exact -- $args -class] < 0} {
+            if {[lsearch -exact $args -class] < 0} {
                 error [_ "The -class option must be specified!"]
             }
             install engine using starships::StarshipEngine %AUTO% \
@@ -504,9 +669,113 @@ namespace eval starships {
                   -size [from args -sizeofmissle mark1]
             $self configurelist $args
         }
+        
+        method statusreport {} {
+            ## Return a status report.
+            #
+            # @return A status report.
             
+            set report [list]
+            foreach {c} [$self configure] {
+                foreach {o n N def val} $c {break}
+                lappend report $N $val
+            }
+            lappend report AvailableLaunchers [$self launchersavailable]
+            lappend report AvailableMissles [$self misslesavailable]
+            lappend report ReloadSpaceAvailable [$self reloadspaceavailable]
+            return $report                                                     
+        }
+        typemethod destroyer {name} {
+            ## Create a destroyer.  Destroyer accelerate at up to \f$.1c\f$,
+            # and have 4 Mark1 launchers.
+            #
+            # @param name The name of the destroyer.
+            # @return A destroyer class starship.
+            
+            return [$type create $name -class destroyer -maxdesignaccel .1 \
+                    -numberoflaunchers 4 -sizeofmissle mark1]
+        }
+        typemethod lightcrusier {name} {
+            ## Create a lightcrusier.  Lightcrusiers accelerate at up to 
+            # \f$.11c\f$, and have 6 Mark1 launchers.
+            #
+            # @param name The name of the lightcrusier.
+            # @return A lightcrusier class starship.
+            
+            return [$type create $name -class lightcrusier \
+                    -maxdesignaccel .11 -numberoflaunchers 6 \
+                    -sizeofmissle mark1]
+        }
+        typemethod heavycrusier {name} {
+            ## Create a heavycrusier.  Heavycrusiers accelerate at up to 
+            # \f$.12c\f$, and have 8 Mark2 launchers.
+            #
+            # @param name The name of the heavycrusier.
+            # @return A heavycrusier class starship.
+            
+            return [$type create $name -class heavycrusier \
+                    -maxdesignaccel .12 -numberoflaunchers 8 \
+                    -sizeofmissle mark2]
+        }
+        typemethod battlecrusier {name} {
+            ## Create a battlecrusier.  Battlecrusiers accelerate at up to 
+            # \f$.15c\f$, and have 10 Mark2 launchers.
+            #
+            # @param name The name of the battlecrusier.
+            # @return A battlecrusier class starship.
+            
+            return [$type create $name -class battlecrusier \
+                    -maxdesignaccel .15 -numberoflaunchers 10 \
+                    -sizeofmissle mark2]
+        }
+        typemethod dreadnought {name} {
+            ## Create a dreadnought.  Dreadnoughts accelerate at up to 
+            # \f$.18c\f$, and have 15 Mark4 launchers.
+            #
+            # @param name The name of the dreadnought.
+            # @return A dreadnought class starship.
+            
+            return [$type create $name -class dreadnought \
+                    -maxdesignaccel .18 -numberoflaunchers 15 \
+                    -sizeofmissle mark4]
+        }
+        typemethod superdreadnought {name} {
+            ## Create a super dreadnought.  Super Dreadnoughts accelerate at 
+            # up to \f$.2c\f$, and have 25 Mark4 launchers.
+            #
+            # @param name The name of the super dreadnought.
+            # @return A super dreadnought class starship.
+            
+            return [$type create $name -class superdreadnought \
+                    -maxdesignaccel .2 -numberoflaunchers 25 \
+                    -sizeofmissle mark4]
+        }
+        typemethod ammunition {name} {
+            ## Create an ammunition ship.  Ammunition ships accelerate at up 
+            # to \f$.2c\f$, and have no missle launchers.
+            #
+            # @param name The name of the ammunition ship.
+            # @return A ammunition ship class starship.
+            
+            return [$type create $name -class ammunition \
+                    -maxdesignaccel .2  -numberoflaunchers 0]
+        }
+        typemethod troopcarrier {name} {
+            ## Create a troop carrier.  Troop Carriers accelerate at up to 
+            # \f$.15c\f$, and have no missle launchers.
+            #
+            # @param name The name of the troop carrier.
+            # @return A troop carrier class starship.
+            
+            return [$type create $name -class troopcarrier \
+                    -maxdesignaccel .15 -numberoflaunchers 0]
+        }
+        
+        
+        
+        
+        
     }
-
 }
 
 
